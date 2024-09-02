a3144db398425894a0603bff2f1e6921fa6d7333
Tanjina Akter Sumi <142243563+sumi52@users.noreply.github.com>
2024-09-02 21:40:04 +0600

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,26 +19,23 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: const Text('Hero Animation'),
         ),
-      body: ListView.builder(  //ListView defult constructor r Listview.builder constructor er eki kaj jst ekhane barti sudhu index
-        itemCount: hotels.length,  //ekekta hotel er jonno ekekta widget banabo,then index dhore dhore oi collection
-                                 // er jonno hotel er object ber korbo & information gulo show korbo,& e dhoroner kaj gulo always
-                                 //aladha widget baniye korbo jmn: lib e hotel_item name e dart file nibo
+      body: ListView.builder(  
+        itemCount: hotels.length,  
         itemBuilder: (context, index) {
-          return HotelItem(hotel: hotels[index]); //[index]parameter hisabe je hiotel er object ta dite hobe se hocche hotels je
-                    //collection er index,tar mane 0num hotel,1 num hotel etc e je index ache serial vabe,egulo purota e ekta hotel
+          return HotelItem(hotel: hotels[index]); 
         },
       ),
     );
   }
   ListView buildListView() {
-    return ListView(      // alada method hisabe thaklo
-      //scrollDirection: Axis.horizontal,  // ager moto vartically rakhte chai tai eta k desible kore dilam
-      children: items.map((item) => SizedBox(          //ekhon sizedBox er o poyojon nai tao rakhlam
-        width: size.width, // vertically kore dichi tai size.width na dile o hoto
+    return ListView(     
+      //scrollDirection: Axis.horizontal,  
+      children: items.map((item) => SizedBox(          
+        width: size.width, 
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ListTile(        //tileVColor gulo buja jacche na tai ListTile ke (alt + enter) wrap with padding er modhe rakhbo
-            tileColor: Colors.grey,  //list gulor color dilam, ekhon SizedBox na dile o chole tao rakhlam
+          child: ListTile(       
+            tileColor: Colors.grey,  
             title: Text(item),
           ),
         ),
